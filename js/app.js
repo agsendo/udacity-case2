f4c644cd3665b065d83b59295fc4fe33f8421957
Aga Sen <azsendero@gmail.com>
2023-04-30 18:39:04 +0100

    Add section headings to the top nav using JS

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@
  * Define Global Variables
  * 
 */
-
+let headers;
 
 /**
  * End Global Variables
@@ -39,7 +39,63 @@
 */
 
 // build the nav
+/*function buildNav() {*/
+    /* get and store all of the section elements*/
+    /*function getSections() {
+    sections = document.getElementsByTagName('section');
+    
+    console.log(sections);
+    }*/
 
+    /*create li element for each section and add to the ul*/
+    /*function addSectionsToNav(sections) {
+        let navbarElement = document.querySelector('#navbar__list');
+        console.log(navbarElement);
+        for (let i = 0; i < sections.length; i++) {
+            let listItem = document.createElement('li');
+            let sectionHeading = sections[i].getElementsByTagName('h2');
+            listItem.innerHTML = sectionHeading.textContent;
+            navbarElement.appendChild(listItem);
+
+            console.log(listItem);
+            console.log(sectionHeading.textContent);
+            console.log(listItem.innerHTML);
+        }
+    }
+
+    getSections();
+    addSectionsToNav(sections);
+}
+
+buildNav();*/
+
+
+
+// build the nav
+function buildNav() {
+
+    //get and store all of the headers-2 elements
+    function getSectionHeaders() {
+    headers = document.getElementsByTagName('h2');
+    }
+
+    //create li element for each section header and add to the ul
+    function addHeadersToNav(headers) {
+        let navbarElement = document.querySelector('#navbar__list');
+        for (let i = 0; i < headers.length; i++) {
+            let listItem = document.createElement('li');
+            listItem.textContent = headers[i].textContent;
+            navbarElement.appendChild(listItem);
+
+            console.log(listItem.textContent);
+        }
+    }
+
+    getSectionHeaders();
+    addHeadersToNav(headers);
+}
+
+buildNav();
 
 // Add class 'active' to section when near top of viewport
 
