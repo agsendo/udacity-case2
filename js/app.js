2682ac65c34536e78ff9ee185c75f68a4ffbdb91
Aga Sen <azsendero@gmail.com>
2023-05-15 21:39:09 +0100

    Add JS for creating navbar and scrolling

--- js/app.js
+++ js/app.js
@@ -1,43 +1,31 @@
-/**
- * 
- * Manipulating the DOM exercise.
+/* Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
-/**
+
+/*
  * Define Global Variables
- * 
+ *
 */
 let headers;
 let sections;
 let sectionIds = [];
-/**
- * End Global Variables
- * Start Helper Functions
- * 
+
+
+
+/*
+ * Define helper functions
+ *
 */
-
-
-//get and store all of the headers h2 elements
+// Get and store all of the headers h2 elements
 function getSectionHeaders() {
     headers = document.getElementsByTagName('h2');
 }
 
-//get and store all of the section IDs (to use for links)
+// Get and store all of the section IDs (to use for links)
 function getSectionIds() {
     sections = document.getElementsByTagName('section');
     for (i = 0; i < sections.length; i++) {
@@ -45,47 +33,42 @@ function getSectionIds() {
     }
 }
 
-//create li element for each section header
-//and add it to the ul with its proper link
+// Create li element for each section header
+// and add it to the ul with its proper link
 function addHeadersToNav(headers) {
-    let navbarElement = document.querySelector('#navbar__list');
+    let navbarElement = document.querySelector('#navbar-list');
     for (let i = 0; i < headers.length; i++) {
         let listItem = document.createElement('li');
-        listItem.innerHTML = `<a href="#${sectionIds[i]}" class="menu__link">${headers[i].textContent}</a>`;
+        listItem.innerHTML = `<a href="#${sectionIds[i]}" class="menu-link">${headers[i].textContent}</a>`;
         navbarElement.appendChild(listItem);
     }
 }
 
-
-//scrolls to the element with given ID
+// Scrolls to the element with given ID
 function scrollToSection(sectionId) {
     let element = document.getElementById(sectionId);
-    element.scrollIntoView({behavior: 'smooth'});
+    element.scrollIntoView({block: 'start', behavior: 'smooth'});
 }
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
+
+/*
+ * Define main functions
+ *
 */
-
-
-// build the nav
+// Build the nav
 function buildNav() {
     getSectionHeaders();
     getSectionIds();
     addHeadersToNav(headers);
 }
 
-
 // Add class 'active' to section when near top of viewport
 
-
-// Scroll to anchor ID using scrollTO event
+// Scroll to anchor ID using scrollTo event
 // Add listener for each nav link
 function addListenersForNav() {
-    let navLinks = document.querySelectorAll('.menu__link');
+    let navLinks = document.querySelectorAll('.menu-link');
     for (let navLink of navLinks) {
         let hrefValue = navLink.getAttribute("href");
         hrefValue = hrefValue.slice(1); //removes the # from the link
@@ -96,12 +79,12 @@ function addListenersForNav() {
     }
 }
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
 
+
+/*
+ * Begin events
+ *
+*/
 // Build menu 
 buildNav();
 
