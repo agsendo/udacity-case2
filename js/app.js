0878a966ac00ef15428c0e52dde6c1c1a02d1ee6
Aga Sen <azsendero@gmail.com>
2023-05-17 18:15:42 +0100

    Make the JS easy to read, add comments

--- js/app.js
+++ js/app.js
@@ -13,8 +13,6 @@
 let headers;
 let sections;
 let sectionIds = [];
-let heightW;
-const heightWRange = 0.4;
 let listitems;
 
 
@@ -28,7 +26,7 @@ function getSectionHeaders() {
     headers = document.getElementsByTagName('h2');
 }
 
-// Get and store all of the section IDs (to use for links)
+// Get and store all of the sections' IDs (to use for links)
 function getSectionIds() {
     sections = document.getElementsByTagName('section');
     for (i = 0; i < sections.length; i++) {
@@ -53,7 +51,7 @@ function scrollToSection(sectionId) {
     element.scrollIntoView({block: 'start', behavior: 'smooth'});
 }
 
-// Get and store all of the li elements from navbar
+// Get and store all of the li link elements from navbar
 function getNavElements() {
     listitems = document.querySelectorAll('.navbar-menu a');
 }
@@ -66,6 +64,43 @@ function setNavElementActive(element) {
 // Remove class 'active' from navbar list item
 function setNavElementNotActive(element) {
     element.classList.remove("active");
+}
+
+// Check the position of all sections
+// Add class 'active' when section is in the viewport and remove otherwise
+function checkRect(sections) {
+    getNavElements();
+    for (let i = 0; i < sections.length; i++) {
+        let rect = sections[i].getBoundingClientRect();
+        if ((rect.top >= 0) && (rect.top <= (window.innerHeight * 0.45)) || 
+        ((rect.bottom <= window.innerHeight) && (rect.bottom > window.innerHeight * 0.4)) ||
+        ((rect.top < 0) && (rect.bottom > window.innerHeight))) {
+            setNavElementActive(listitems[i]);
+        } else {setNavElementNotActive(listitems[i])}
+    }
+}
+
+// Make the button visible when the displayed content is below the fold
+// by checking if the first section is currently at the top of the viewport
+function addListenerForBelowFold() {
+    document.addEventListener('scroll', function() {
+        let section1 = document.querySelector('#section1');
+        let btn = document.querySelector('#btn-top');
+        let rectS = section1.getBoundingClientRect();
+        if (rectS.top < 100) {
+            btn.style.visibility = "visible";
+        } else {
+            btn.style.visibility = "hidden";
+        }
+    })
+}
+
+// Scroll smoothly to the top when the button is clicked
+function addlistenerScrollTop() {
+    let btn = document.querySelector('#btn-top');
+    btn.addEventListener('click', function() {
+        window.scrollTo({ top:0, behavior: 'smooth' });
+    })
 }
 
 
@@ -96,7 +131,18 @@ function addListenersForNav() {
 }
 
 // Add class 'active' to section when near top of viewport
+function addListenerForScroll() {
+    sections = document.getElementsByTagName('section');
+    document.addEventListener('scroll', function() {
+        checkRect(sections);
+    });
+}
 
+// Show the button when below the fold and scroll to the top when clicked
+function scrollToTopButton() {
+    addListenerForBelowFold();
+    addlistenerScrollTop();
+}
 
 
 
@@ -111,90 +157,7 @@ buildNav();
 addListenersForNav();
 
 // Set sections as active
-
-
-
-
-
-
-
-
-//
-//
-// Checks the window's height when resized
-/*function addListenerForResize() {
-    window.addEventListener('resize', function () {
-        heightW = window.innerHeight;
-    });
-}
-
-addListenerForResize();
-*/
-
-
-
-
-getNavElements();
-//
-function checkRect(sections) {
-    for (let i = 0; i < sections.length; i++) {
-        let rect = sections[i].getBoundingClientRect();
-        console.log(rect.top);
-        /*console.log(window.innerHeight * 0.5);*/
-        if ((rect.top >= 0) && (rect.top <= (window.innerHeight * 0.45)) || 
-        ((rect.bottom <= window.innerHeight) && (rect.bottom > window.innerHeight * 0.4)) ||
-        ((rect.top < 0) && (rect.bottom > window.innerHeight))) {
-            console.log(i+1);
-            setNavElementActive(listitems[i]);
-        } else {setNavElementNotActive(listitems[i])}
-    }
-}
-/*checkRect(sections);*/
-
-function addListenerForScroll() {
-    document.addEventListener('scroll', function() {
-        checkRect(sections);
-    });
-}
-
 addListenerForScroll();
 
-
-/*
-function getViewportSize() {
-   let width = window.innerWidth;
-   let height = window.innerHeight;
-   console.log(`the width is ${width} and height is ${height}`);
-}
-
-getViewportSize();
-
-
-function addListenersForNav() {
-    let navLinks = document.querySelectorAll('.menu-link');
-    for (let navLink of navLinks) {
-        let hrefValue = navLink.getAttribute("href");
-        hrefValue = hrefValue.slice(1); //removes the # from the link
-        navLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            scrollToSection(`${hrefValue}`);
-        });
-    }
-}
-*/
-
-
-function addListenerForButton() {
-    document.addEventListener('scroll', function() {
-        let section1 = document.querySelector('#section1');
-        let btn = document.querySelector('.scroll-top button');
-        let rectS = section1.getBoundingClientRect();
-        if (rectS.top < 100) {
-            btn.style.visibility = "visible";
-        } else {
-            btn.style.visibility = "hidden";
-        }
-    })
-}
-
-addListenerForButton();
+// Scroll to the top button
+scrollToTopButton();
